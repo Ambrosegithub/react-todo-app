f7627d1657a4e5b479b3526552635bb3a9105771
Isigba Ambrose <ambrisexyz@gmail.com>
2022-08-19 19:31:10 +0200

    add functional component

---
+++ src/components/InputTodo.js
@@ -0,0 +1,132 @@
+import React, { useState } from "react"
+const InputTodo = (props) => {
+  const [inputText, setInputText] = useState({
+    title: "",
+  })
+
+  const onChange = e => {
+    setInputText({
+      ...inputText,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(inputText.title.trim()) {
+      props.addTodoProps(inputText.title);
+      setInputText({
+        title: "",
+      })
+    } else {
+      alert("Please write item")
+    } 
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <input
+        type="text"
+        className="input-text"
+        placeholder="Add todo..."
+        value={inputText.title}
+        name="title"          
+        onChange={onChange}
+      />
+      <button className="input-submit">
+              </button>
+    </form>
+  )
+}
+
+export default InputTodo
+/*
+<FaPlusCircle style={{ color: "darkcyan", fontSize: "20px", marginTop: "2px" }}/>
+
+import React, { useState } from 'react';
+
+
+const InputTodo = (props) => {
+  const [inputText, setInputText] = useState({
+    title: '',
+  });
+
+  const onChange = (e) => {
+    setInputText({
+      ...inputText,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputText.title.trim()) {
+      props.addTodoProps(inputText.title);
+      setInputText({
+        title: '',
+      });
+    } else {
+      alert('Please write item');
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <input
+        type="text"
+        className="input-text"
+        placeholder="Add Todo..."
+        value={inputText.title}
+        name="title"
+        onChange={onChange}
+      />
+      <button className="input-submit">Submit</button>
+    </form>
+  );
+};
+
+export default InputTodo;
+*/
+//import { FaPlusCircle } from "react-icons/fa";
+
+/*
+import React, { Component } from "react"
+
+class InputTodo extends Component {
+    state = {
+        title: ""
+      };
+      onChange = e => {
+        this.setState({
+          [e.target.name]: e.target.value,
+        }) 
+           };
+
+           handleSubmit = e => {
+            e.preventDefault()
+            if (this.state.title.trim()) {
+              this.props.addTodoProps(this.state.title)
+              this.setState({
+                title: "",
+              })
+            } else {
+              alert("Please write item")
+            }
+          }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className="form-container">
+  <input
+    type="text"
+    className="input-text"
+    placeholder="Add todo..."
+    value={this.state.title}
+    name="title"
+    onChange={this.onChange}
+  />
+  <button className="input-submit">Submit</button>
+</form>
+    )
+  }
+}
+export default InputTodo
+*/
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from "react"
+import ReactDOM from "react-dom/client";
+//component file
+import TodoContainer from "./components/TodoContainer.js";
+import './App.css'
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <TodoContainer />
+  </React.StrictMode>
 );
